16fb303fe2256871b21a91b2d3b8021f166c1189
Sandipan Ghosh <sandipan.ghosh2005@gmail.com>
2018-08-20 23:31:51 -0400

    Dependency injection using fields - annotations.

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+	@Autowired
 	private FortuneService fortuneService;
 	
 	/*
@@ -19,11 +20,13 @@ public class TennisCoach implements Coach {
 		System.out.println("TennisCoach: Inside default constructor.");
 	}
 	
+	/*
 	@Autowired
 	public void setFortuneService(FortuneService theFortuneService) {
 		System.out.println("TennisCoach: Inside setFortuneService");
 		fortuneService = theFortuneService;
 	}
+	*/
 	
 	@Override
 	public String getDailyWorkout() {
