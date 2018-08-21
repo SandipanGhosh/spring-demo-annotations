942b95d790b3e044b4fd8bf3c5f2864cc8933b8f
Sandipan Ghosh <sandipan.ghosh2005@gmail.com>
2018-08-21 00:03:40 -0400

    Dependency injection with qualifier.

---
+++ src/com/luv2code/springdemo/RandomFortuneService.java
@@ -0,0 +1,25 @@
+package com.luv2code.springdemo;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomFortuneService implements FortuneService {
+
+	private String[] data = {
+			"fortune1",
+			"fortune2",
+			"fortune3"
+	};
+	
+	private Random myRandom = new Random();
+	
+	@Override
+	public String getFortune() {
+		int index = myRandom.nextInt(data.length);
+		String theFortune = data[index];
+		return theFortune;
+	}
+
+}
