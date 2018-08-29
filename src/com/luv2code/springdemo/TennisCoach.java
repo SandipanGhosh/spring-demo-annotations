a84891b75bb02a6c7726a1bf86db1c308d409867
Sandipan Ghosh <sandipan.ghosh2005@gmail.com>
2018-08-28 23:32:00 -0400

    Post construct and Pre destroy

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,7 +1,11 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,6 +14,16 @@ public class TennisCoach implements Coach {
 	@Autowired
 	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
+	
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println("TennisCoach: inside doMyStartupStuff.");
+	}
+	
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println("TennisCoach: inside doMyCleanupStuff");
+	}
 	
 	/*
 	@Autowired
